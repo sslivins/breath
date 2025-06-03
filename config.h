794ab41e928e8f4d9611ceb1895f51a29f981821
Stefan Slivinski <sslivins@gmail.com>
2025-06-02 20:28:23 -0700

    bump version

--- config.h
+++ config.h
@@ -12,7 +12,7 @@
 #define RESET_WIFI_PIN 4 // Pin to reset WiFi credentials (if needed)
 #define DONE_PIN 7
 
-#define FW_VERSION "0.1.10"
+#define FW_VERSION "0.1.11"
 #define IMAGE_MANIFEST_URL "https://github.com/sslivins/breath/releases/latest/download/manifest.json"
 
 #endif
