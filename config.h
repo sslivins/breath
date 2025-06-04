083a05ad31fef17485ba0366c4b54cd0a212fe84
Stefan Slivinski <sslivins@gmail.com>
2025-06-04 12:11:52 -0700

    try optimizing for size and removing debug code

--- config.h
+++ config.h
@@ -7,10 +7,6 @@
 #define ENABLE_SERIAL_DEBUG  // Enable serial debug output
 
 #define RED_LED LED_BUILTIN
-#define BOOT_BUTTON_PIN 9
-#define NEOPIXEL_PIN 9   // Feather ESP32-C6 NeoPixel is on GPIO 9
-#define NUMPIXELS    1   // There's only one NeoPixel on the board
-
 #define RESET_WIFI_PIN 4 // Pin to reset WiFi credentials (if needed)
 #define DONE_PIN 7
 
