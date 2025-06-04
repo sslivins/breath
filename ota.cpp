877e117a137665a9f921b8934d400a8e37de5672
Stefan Slivinski <sslivins@gmail.com>
2025-06-03 19:40:59 -0700

    add ability to enable/disable prints

--- ota.cpp
+++ ota.cpp
@@ -13,7 +13,9 @@ void OTAUpdater::checkAndUpdate() {
     int httpCode = http.GET();
     if (httpCode != 200) {
         http.end();
+#ifdef ENABLE_SERIAL_DEBUG
         Serial.printf("Failed to fetch manifest: %d\n", httpCode);
+#endif
         return;
     }
 
@@ -25,8 +27,10 @@ void OTAUpdater::checkAndUpdate() {
     String latestVersion = doc["version"];
     String binUrl = doc["bin_url"];
     if (isNewerVersion(latestVersion, FW_VERSION)) {
+#ifdef ENABLE_SERIAL_DEBUG
         Serial.printf("New version available: %s (current: %s)\n", latestVersion.c_str(), FW_VERSION);
         Serial.printf("Downloading update from: %s\n", binUrl.c_str());
+#endif
         performOTA(binUrl);
     }
 }
@@ -36,28 +40,38 @@ bool OTAUpdater::isNewerVersion(const String& remote, const String& local) {
 }
 
 void OTAUpdater::performOTA(const String& binUrl) {
+#ifdef ENABLE_SERIAL_DEBUG
     Serial.println("Starting OTA update...");
+#endif
     HTTPClient http;
     http.setFollowRedirects(HTTPC_STRICT_FOLLOW_REDIRECTS);
     http.begin(binUrl);
     int httpCode = http.GET();
     if (httpCode != 200) {
+#ifdef ENABLE_SERIAL_DEBUG
         Serial.printf("Failed to download firmware: %d\n", httpCode);
+#endif
         http.end();
         return;
     }
 
     int contentLength = http.getSize();
+#ifdef ENABLE_SERIAL_DEBUG
     Serial.printf("Firmware size: %d bytes\n", contentLength);
+#endif
     bool canBegin = Update.begin(contentLength);
     if (!canBegin) {
+#ifdef ENABLE_SERIAL_DEBUG
         Serial.println("Not enough space to begin OTA");
+#endif
         http.end();
         return;
     }
 
     WiFiClient* stream = http.getStreamPtr();
+#ifdef ENABLE_SERIAL_DEBUG
     Serial.println("Writing firmware to flash...");
+#endif
 
     Update.onProgress([](size_t written, size_t total) {
         // Example: Blink LED on each progress callback
@@ -74,16 +88,28 @@ void OTAUpdater::performOTA(const String& binUrl) {
 
     // Turn off LED after update
     digitalWrite(RED_LED, LOW);
+#ifdef ENABLE_SERIAL_DEBUG
     Serial.printf("Written %u/%u bytes\n", (unsigned)written, (unsigned)contentLength);
+#endif
     if (written == contentLength && Update.end()) {
         if (Update.isFinished()) {
+#ifdef ENABLE_SERIAL_DEBUG
             Serial.println("OTA update successful. Restarting...");
+#endif
             ESP.restart();
-        } else {
+        } 
+#ifdef ENABLE_SERIAL_DEBUG
+        else {
+
             Serial.println("OTA update not finished.");
         }
-    } else {
+#endif        
+    } 
+#ifdef ENABLE_SERIAL_DEBUG    
+    else {
+
         Serial.printf("OTA update failed. Error #: %d\n", Update.getError());
     }
+#endif    
     http.end();
 }
